56b81e502b63c40bf38a09547cd1fd0b88990623
Karl Cui <karl.cui@mail.utoronto.ca>
2021-01-24 15:20:42 -0800

    edit pipeline to add html to db entry upon crawler completion

--- crawler/pipelines.py
+++ crawler/pipelines.py
@@ -7,18 +7,24 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 import pymongo
+from uuid import UUID
+
+
 MongoClient = pymongo.MongoClient
+client = MongoClient("localhost", 27017)
+db = client.yomou
+
 
 # intercept result to save it to mongodb 'htmlpages' collection
 class CrawlerPipeline:
     def process_item(self, item, spider):
-        client = MongoClient("localhost", 27017)
-        db = client.yomou
-        unique_id = spider.unique_id
-        id = db.htmlpages.insert_one({
-            "unique_id": unique_id,
-            "html": item.get('res')
-        }).inserted_id
-        print(id)
+        # convert UUID string to mongo-compatible repr
+        # see https://stackoverflow.com/a/60404331
+        uuid = UUID(spider.uuid)
+
+        # add html to entry with specified UUID in db
+        query = {"uuid": uuid}
+        update = {"$set": {"html": item.get("res")}}
+        db.documents_document.update_one(query, update)
 
         return item
